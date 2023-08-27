c5efbb04c161e09fd160354326622b25a0abe173
Peter Svensson <psvensson@gmail.com>
2023-08-27 19:48:59 +0200

    working sample state transfer

--- src/RaftRunner.js
+++ src/RaftRunner.js
@@ -1,42 +1,81 @@
 const raft = require('zmq-raft');
 const RunnerStateMachine = require('./RunnerStatemachine');
+const { listeners } = require('process');
 
 module.exports = class RaftRunner {
     constructor(id, path, port, peers, stateHandler) {
         this.setUpProcessHandlers()
-        const options = this.getOptions(id, path, port, peers,stateHandler)
+        const options = this.getOptions(id, path, port, peers, stateHandler)
         console.log('-------------------- builder options: ', options)
         raft.server.builder.build(options).then(zmqRaft => {
             this.zmqRaft = zmqRaft;
             const peers = this.zmqRaft.peers;
-            console.log('raft peers: ', peers)
+            console.log('raft peers: ', peers)            
         });
+        // DEBUG
+        setInterval(this.handleInterval.bind(this), 15000)
+        // ------------------------
+        const clientSeedPeers = peers.map(peer=>peer.url)
+        this.client = new raft.client.ZmqRaftClient(clientSeedPeers, {
+            secret: '', lazy: true, heartbeat: 5000
+        });
+        
+    }
+
+    handleInterval() {
+        console.log('--- handleInterval role is; ',this.raftState.toString())
+        if (this.raftState.toString() === 'Symbol(Leader)') {
+            // Send sample message throught the client
+            console.log('--- sending message to all peers')
+            this.clientSend('hello world')
+        }
+    }
+
+    handleRaftState(state, term) {
+        console.log('--- handleRaftState: ', state, term)
+        this.raftState = state
+    }
+
+    async clientSend(text) {
+        const serializedTxData = Buffer.from(JSON.stringify(text));
+        const requestId = raft.utils.id.genIdent();
+        const logIndex = await this.client.requestUpdate(requestId, serializedTxData);
     }
 
     getOptions(id, path, port, peers, stateHandler) {
         const ipAddr = this.getExternalIp()
         const myAddr = this.getUrlFor(ipAddr, port)
-        const myWWW = this.getWwwFor(ipAddr, parseInt(port)+1)
-        const defaultPeers = [{ id: "id1", url: myAddr, www: myWWW }]
+        const myWWW = this.getUrlFor(ipAddr, parseInt(port) + 1)
+        const myPub = this.getUrlFor(ipAddr, parseInt(port) + 2)
+        const defaultPeers = [{ id: "id1", url: myAddr, www: myWWW, pub: myPub }]
         const options = {
             id: id,
             secret: "",
             peers: peers || defaultPeers,
             data: {
-                path: path
+                path: path,
+                appendIdToPath: true
             },
             router: {
-                bind: 'tcp://*:' + port,
+                //bind: 'tcp://*:' + port,
+                bind: myAddr,
+            },
+            broadcast: {
+                /* required for broadcast state */
+                url: myPub,
             },
             factory: {
                 state: (options) => {
                     options.stateHandler = stateHandler;
                     return new RunnerStateMachine(options)
                 }
+            },
+            listeners:{
+                state: this.handleRaftState.bind(this)
             }
         }
-        if(options.peers[0].www) {
-            options.webmonitor = {enable: true, host: ipAddr, port: parseInt(port)+1}
+        if (options.peers[0].www) {
+            options.webmonitor = { enable: true, host: ipAddr, port: parseInt(port) + 1 }
         }
         return options
     }
@@ -44,11 +83,6 @@ module.exports = class RaftRunner {
     getUrlFor(address, port) {
         return `tcp://${address}:${port}`;
         //return `tcp://127.0.0.1:${port}`;
-    }
-
-    getWwwFor(address, port) {
-        return `http://${address}:${port}`;
-        //return `http://127.0.0.1:${port}`;
     }
 
     // Get first external IP address of this machine as a string
@@ -80,7 +114,7 @@ module.exports = class RaftRunner {
             })
             .on('SIGINT', () => {
                 console.debug('terminating * SIGINT *');
-                shutdown();
+                this.shutdown();
             })
             .on('SIGHUP', () => {
                 console.debug('terminating * SIGHUP *');
--- src/RunnerStatemachine.js
+++ src/RunnerStatemachine.js
@@ -1,34 +1,40 @@
 const raft = require('zmq-raft');
 
-module.exports = class RunnerStateMachine extends raft.api.StateMachineBase{
-    constructor(options) {
-        super();
-        this.initMyStateMachine().then(() => {
-          this.stateHandler = options.stateHandler
-          this[Symbol.for('setReady')]();
-        });
-      }
+module.exports = class RunnerStateMachine extends raft.api.StateMachineBase {
+  constructor(options) {
+    super();
+    this.initMyStateMachine().then(() => {
+      this.stateHandler = options.stateHandler
+      this[Symbol.for('setReady')]();
+    });
+  }
 
-      async initMyStateMachine() {
-        // ... do something
-      }
+  async initMyStateMachine() {
+    console.log('--- intializing my state machine')
+    
+    
+  }
+
   
-      applyEntries(logEntries, nextIndex, currentTerm, snapshot) {
-        for (let [index, item] of logEntries.entries()) {
-            let entry = raft.common.LogEntry.bufferToLogEntry(item, nextIndex + index);
-            console.log("log entry: log-index=%s term=%s", entry.logIndex, entry.readEntryTerm());
-            if (entry.isStateEntry) {
-              console.log("this is state entry:");
-              //  user data of the log entry
-              let data = entry.readEntryData();
-              this.stateHandler.handle(data)
-              // ... do something with entry data
-            } else if (entry.isConfigEntry) {
-              console.log("this is config entry");
-            } else {
-              console.log("this is checkpoint entry");
-            }
-        }
-        return super.applyEntries(logEntries, nextIndex, currentTerm, snapshot);
+
+  applyEntries(logEntries, nextIndex, currentTerm, snapshot) {
+    for (let [index, item] of logEntries.entries()) {
+      let entry = raft.common.LogEntry.bufferToLogEntry(item, nextIndex + index);
+      console.log("log entry: log-index=%s term=%s", entry.logIndex, entry.readEntryTerm());
+      if (entry.isStateEntry) {
+        console.log("this is state entry:");
+        //  user data of the log entry
+        let data = entry.readEntryData();
+        this.stateHandler.handle(data)
+        // ... do something with entry data
+      } else if (entry.isConfigEntry) {
+        console.log("this is config entry");
+      } else {
+        console.log("this is checkpoint entry");
       }
+    }
+    return super.applyEntries(logEntries, nextIndex, currentTerm, snapshot);
+  }
+
+
 }
